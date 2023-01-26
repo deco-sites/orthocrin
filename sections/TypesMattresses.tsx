560cb479a14359f1179740da85d266ea90864b84
Lucas Lima <lucaslima02@mail.uft.edu.br>
2023-01-26 13:59:46 -0300

    feat: add type mattresses section

--- sections/TypesMattresses.tsx
+++ sections/TypesMattresses.tsx
@@ -2,6 +2,9 @@ function TypesMattresses() {
   const style = {
     boxShadow: '0 0 14px 0 #00000024',
   };
+  const text = {
+    'text-align': 'center',
+  };
   return (
     <div class="mx-[25px] mt-[-157px]">
       <div style={style} class="rounded-b-[10px] rounded-t-[10px] bg-[#fff]">
@@ -14,38 +17,42 @@ function TypesMattresses() {
         <div class="flex w-full mt-[-65px] h-[240px]">
           <a href="#" class="w-[33%] flex flex-col items-center">
             <img
-              class="w-[125px] h-[125px]"
+              class="w-[125px] h-[125px] object-contain"
               src="/mattress-type1.png"
               alt="colchão - mola"
             />
             <h6 class="text-[.875rem]">
               <strong>Mola</strong>
             </h6>
-            <p class="text-[.875rem] text-[#8c8c8c]">
+            <p style={text} class="text-[.875rem] text-[#8c8c8c]">
               Ideal para casais, mais durável
             </p>
           </a>
           <a href="#" class="w-[33%] flex flex-col items-center">
             <img
-              class="w-[125px] h-[125px]"
+              class="w-[125px] h-[125px] object-contain"
               src="/mattress-type2.png"
               alt="colchão - espuma"
             />
             <h6 class="text-[.875rem]">
               <strong>Espuma</strong>
             </h6>
-            <p class="text-[.875rem] text-[#8c8c8c]">Conforto e firmeza</p>
+            <p style={text} class="text-[.875rem] text-[#8c8c8c]">
+              Conforto e firmeza
+            </p>
           </a>
           <a href="#" class="w-[33%] flex flex-col items-center">
             <img
-              class="w-[125px] h-[125px]"
+              class="w-[125px] h-[125px] object-contain"
               src="/mattress-type3.png"
               alt="colchão - ortopédico"
             />
             <h6 class="text-[.875rem]">
               <strong>Ortopédico</strong>
             </h6>
-            <p class="text-[.875rem] text-[#8c8c8c]">Conforto extra firme</p>
+            <p style={text} class="text-[.875rem] text-[#8c8c8c]">
+              Conforto extra firme
+            </p>
           </a>
         </div>
       </div>
