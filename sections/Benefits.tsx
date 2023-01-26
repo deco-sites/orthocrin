d3d12d2f0ee568bf5d020ab493ab16d8fc96978e
Lucas Lima <lucaslima02@mail.uft.edu.br>
2023-01-26 12:14:00 -0300

    feat: add benefits section

---
+++ sections/Benefits.tsx
@@ -0,0 +1,35 @@
+function Benefits() {
+  return (
+    <div class="w-full bg-[#fcf5ea] pb-[175px] pt-[15px]">
+      <div class="flex-row flex mx-[25px] gap-[15px]">
+        <div class="flex-col flex items-start">
+          <img src="/benefit-1.webp" alt="Frete grátis" />
+          <p class="text-[.75rem] mt-[10px]">
+            <strong>Frete grátis</strong> em entregas e devoluções
+          </p>
+        </div>
+        <div class="flex-col flex items-start">
+          <img src="/benefit-2.webp" alt="Maior fábrica da América latina" />
+          <p class="text-[.75rem] mt-[10px]">
+            <strong>Maior fábrica</strong> da América Latina
+          </p>
+        </div>
+        <div class="flex-col flex items-start">
+          <img src="/benefit-3.webp" alt="13% off" />
+          <p class="text-[.75rem] mt-[10px]">
+            <strong>13% off </strong>à vista ou
+            <strong> até 10x sem juros</strong>
+          </p>
+        </div>
+        <div class="flex-col flex items-start">
+          <img src="/benefit-4.webp" alt="Frete grátis" />
+          <p class="text-[.75rem] mt-[10px]">
+            <strong>Qualidade de sono superior</strong> Pró-Espuma
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Benefits;
