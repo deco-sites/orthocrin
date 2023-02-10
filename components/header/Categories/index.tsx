cd32cde821dce582c27317bb3fcbb961200a0912
Pedro Henrique de V. A <pedrovasalmeida@gmail.com>
2023-02-10 16:58:20 -0300

    feat: :sparkles: create buttons and categories for header desktop

--- components/header/Categories/index.tsx
+++ components/header/Categories/index.tsx
@@ -9,7 +9,9 @@ export function Categories() {
           className="flex group hover:bg-red-600 flex-col px-4 items-center justify-center w-auto h-[93px] cursor-pointer ease duration-200"
         >
           <img className="w-[30px] h-[40px]" src={item.imageUrl} />
-          <span className="flex flex-nowrap group-hover:text-white text-sm">{item.label}</span>
+          <span className="flex flex-nowrap group-hover:text-white text-sm">
+            {item.label}
+          </span>
         </div>
       ))}
     </div>
