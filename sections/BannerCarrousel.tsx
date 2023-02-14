589fb8084d7796dfa4ec1ccd6a2798b1e14f4e19
Pedro Henrique de V. A <pedrovasalmeida@gmail.com>
2023-02-14 18:53:08 -0300

    fix: :bug: footer styles

--- sections/BannerCarrousel.tsx
+++ sections/BannerCarrousel.tsx
@@ -1,6 +1,6 @@
 export default function BannerCarousel() {
   return (
-    <div class="w-full h-[400px] hidden lg:flex">
+    <div class="w-full h-[400px] hidden lg:flex mt-[264px]">
       <img
         src="https://orthocrin.vteximg.com.br/arquivos/ids/166383/banner-desktop-vega.jpg"
         loading="lazy"
--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -27,11 +27,11 @@ function Footer({ footerLinks }: Props) {
     padding: "45px 25px",
     "row-gap": "35px",
   };
+
   return (
-    <div className="overflow-hidden">
-      <div class="px-[1rem]">
-        <img src="/logo-footer.png" alt="Logo footer" />
-      </div>
+    <div className="overflow-hidden pt-10 px-6">
+      <img src="/logo-footer.png" alt="Logo footer" class="w-[217px]" />
+
       <div style={grid}>
         {footerLinks?.map((footerLink, idx) => (
           <div key={idx}>
@@ -50,24 +50,21 @@ function Footer({ footerLinks }: Props) {
           </div>
         ))}
       </div>
-      <div class="px-[25px] py-[45px]" style={border}>
+      <div class="py-[45px]" style={border}>
         <h6 class="mb-[16px]">
           <strong>Meios de pagamento</strong>
         </h6>
-        <div class="relative max-w-max">
+
+        <div class="relative max-w-full w-full">
           <div
             style={style}
-            class="flex flex-col flex-wrap rounded-t-[10px] rounded-b-[10px]"
+            class="flex flex-col flex-wrap rounded-t-[10px] rounded-b-[10px] w-[calc(100%-70px)]"
           >
-            <p class="flex justify-center mb-[10px] text-[Max(1.65vh,.825rem)]">
-              Pagamento com até <span class="text-[#28b440]">2 cartões</span>
+            <p class="flex justify-center mb-[10px] text-[Max(1.65vh,.825rem)] gap-1">
+              Pagamento com até <span class="text-[#28b440]"> 2 cartões</span>
             </p>
-            <div class="flex flex-row flex-wrap justify-center gap-x-[40px] gap-y-[17px]">
-              <img
-                class="w-auto object-contain"
-                src="/payment-visa.webp"
-                alt="cartão visa"
-              />
+            <div class="flex flex-row flex-wrap justify-center gap-4">
+              <img class="w-auto object-contain" src="/payment-visa.webp" alt="cartão visa" />
               <img
                 class="w-auto object-contain"
                 src="/payment-mastercard.webp"
@@ -83,23 +80,11 @@ function Footer({ footerLinks }: Props) {
                 src="/payment-americanexpress.webp"
                 alt="cartão marican express"
               />
-              <img
-                class="w-auto object-contain"
-                src="/payment-elo.webp"
-                alt="cartão elo"
-              />
-              <img
-                class="w-auto object-contain"
-                src="/payment-diners.webp"
-                alt="club dinner"
-              />
+              <img class="w-auto object-contain" src="/payment-elo.webp" alt="cartão elo" />
+              <img class="w-auto object-contain" src="/payment-diners.webp" alt="club dinner" />
             </div>
           </div>
-          <img
-            class="absolute right-[-79px] bottom-[24px]"
-            src="/card-discount-seal.webp"
-            alt="selo"
-          />
+          <img class="absolute right-0 bottom-[24px]" src="/card-discount-seal.webp" alt="selo" />
         </div>
       </div>
       <div class="px-[25px] py-[45px]" style={border}>
@@ -128,11 +113,7 @@ function Footer({ footerLinks }: Props) {
         </h6>
         <div class="flex justify-between flex-row flex-wrap mt-[20px] items-center">
           <a href="#">
-            <img
-              width={102}
-              src="/x-certificado-ssl.png"
-              alt="certificado ssl"
-            />
+            <img width={102} src="/x-certificado-ssl.png" alt="certificado ssl" />
           </a>
           <a href="#">
             <img width={72} src="/clearsale.webp" alt="clear sale" />
@@ -155,16 +136,16 @@ function Footer({ footerLinks }: Props) {
           </div>
           <div>
             <p class="flex justify-center text-[.75rem] mb-[20px] mt-[40px]">
-              Para conversar sobre proteção e privacidade de dados pessoais
-              (LGPD), entre em contato:{" "}
+              Para conversar sobre proteção e privacidade de dados pessoais (LGPD), entre em
+              contato:{" "}
               <a class="text-[#0000ee]" href="mailto:dpo@orthocrin.com.br">
                 dpo@orthocrin.com.br
               </a>
             </p>
             <p class="flex justify-center text-[.75rem]">
-              Orthocrin Indústria e Comércio Ltda - CNPJ: 17.245.986/0001-62
-              CEP: 33.040.262 End: Rua Lubomir Abadjieff, nº150 - Bairro
-              Distrito Industrial Simão da Cunha - Santa Luzia - MG - Brasil
+              Orthocrin Indústria e Comércio Ltda - CNPJ: 17.245.986/0001-62 CEP: 33.040.262 End:
+              Rua Lubomir Abadjieff, nº150 - Bairro Distrito Industrial Simão da Cunha - Santa Luzia
+              - MG - Brasil
             </p>
           </div>
         </div>
